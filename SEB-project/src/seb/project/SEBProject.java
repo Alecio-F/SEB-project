f878abae5501086fbab5d561c154b80eb25fea0b
Alécio <ciceroalecio30@gmail.com>
2024-10-10 04:06:03 -0300

    Conectando o banco de dados local

--- SEB-project/src/seb/project/SEBProject.java
+++ SEB-project/src/seb/project/SEBProject.java
@@ -2,7 +2,15 @@ package seb.project;
 
 public class SEBProject {
     public static void main(String[] args) {
-
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            if (connection != null){
+                System.out.println("Conexão bem-sucedida!");
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     
 }
