80d4a6625a06c9783d89d72993cb2acdb9462292
Alécio <ciceroalecio30@gmail.com>
2024-10-19 03:42:14 -0300

    Comentarios explicativos

--- SEB-project/src/seb/project/Main.java
+++ SEB-project/src/seb/project/Main.java
@@ -2,6 +2,7 @@ package seb.project;
 
 //import java.sql.Connection;
 //import java.sql.SQLException;
+import java.util.Arrays;
 import seb.project.dao.ClienteDAO;
 import seb.project.model.Cliente;
 import java.util.List;
@@ -27,13 +28,22 @@ public class Main {
         ClienteDAO clienteDAO = new ClienteDAO();
 
         // Criar um cliente (com o modelo)
-        Cliente cliente = new Cliente("João Silva", "12345678901234", "Rua Exemplo, 100", "São Paulo", "SP", "12345-678", "joao@example.com", "11987654321");
+        //Cliente cliente = new Cliente("João Silva", "12345678901234", "Rua Exemplo, 100", "São Paulo", "SP", "12345-678", "joao@example.com", "11987654321");
 
         // Inserir o cliente usando o DAO
-        clienteDAO.inserirCliente(cliente);
-
+        //clienteDAO.inserirCliente(cliente);
+        
+        List<Integer> idsParaDeletar = Arrays.asList(2, 5, 6);
+        clienteDAO.deletarClientes(idsParaDeletar);
         // Listar novamente para ver o efeito das alterações
-        clienteDAO.listarClientes();
-
+        List<Cliente> clientes = clienteDAO.listarClientes();
+        // exibe os clientes no terminal
+        if (clientes.isEmpty()) {
+        System.out.println("Nenhum cliente encontrado.");
+        } else {
+        for (Cliente cli: clientes) {
+        System.out.println(cli.getId() + " - " + cli.getNome() + " - " + cli.getCnpj());
+        }
+        }
     }
 }
