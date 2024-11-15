79e2710d45ac8cd424a3eb22e6ac0c052b1375a2
Alécio <ciceroalecio30@gmail.com>
2024-11-15 18:18:59 -0300

    organização pasta ui

--- SEB-project/src/seb/project/api/GeocodingAPI.java
+++ SEB-project/src/seb/project/api/GeocodingAPI.java
@@ -12,7 +12,7 @@ import org.json.JSONObject;
 public class GeocodingAPI {
 
     private static final String API_URL = "https://maps.googleapis.com/maps/api/geocode/json";
-    private static final String API_KEY = "AIzaSyCQve9Nj0j3TEg1l"; 
+    private static final String API_KEY = "AIzaSyCQve9Nj0j3TE"; 
 
     // Método para obter coordenadas (latitude, longitude) com base no endereço
     public double[] obterCoordenadas(String endereco) {
@@ -30,7 +30,7 @@ public class GeocodingAPI {
 
             // Verificar se a resposta foi bem-sucedida (código 200)
             int responseCode = con.getResponseCode();
-            System.out.println("URL: " + urlString); // Imprimir a URL gerada
+            // System.out.println("URL: " + urlString); // Imprimir a URL gerada
             if (responseCode == 200) {
                 StringBuilder response = new StringBuilder();
                 try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
